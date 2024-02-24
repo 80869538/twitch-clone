d824b34c9bd252cc1aa58255e01b69bc5ff5cf3d
Andrew <andrew.jiang81@gmail.com>
2024-02-25 01:36:05 +1100

    following system

--- app/(browse)/_component/sidebar/wrapper.tsx
+++ app/(browse)/_component/sidebar/wrapper.tsx
@@ -1,12 +1,28 @@
 "use client";
 import {useSidebar} from "@/store/use-sidebar";
 import {cn} from "@/lib/utils";
+import {useEffect, useState} from "react";
+import {ToggleSkeleton} from "./toggle";
+import {RecommendedSkeleton} from "./recommended";
 interface WrapperProps {
   children: React.ReactNode;
 }
 
 export const Wrapper = ({children}: WrapperProps) => {
   const {collapsed} = useSidebar((state) => state);
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient)
+    return (
+      <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+        <ToggleSkeleton />
+        <RecommendedSkeleton />
+      </aside>
+    );
   return (
     <aside
       className={cn(
