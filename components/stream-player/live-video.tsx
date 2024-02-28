c2c804a293671369c09c5f44bc685eded6d26936
Andrew <andrew.jiang81@gmail.com>
2024-02-28 14:55:52 +1100

    add volume and full screen control

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -5,6 +5,7 @@ import {Participant, Track} from "livekit-client";
 import {useTracks} from "@livekit/components-react";
 import {useEventListener} from "usehooks-ts";
 import {FullscreenControl} from "./fullscreen-control";
+import {VolumeControl} from "./volume-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -14,6 +15,8 @@ export const LiveVideo = ({participant}: LiveVideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
   const [isFullscreen, setIsFullscreen] = useState(false);
+  const [volume, setVolume] = useState(0);
+
   const toggleFullscreen = () => {
     if (isFullscreen) {
       document.exitFullscreen();
@@ -26,6 +29,27 @@ export const LiveVideo = ({participant}: LiveVideoProps) => {
     const isCurrentlyFullscreen = document.fullscreenElement !== null;
     setIsFullscreen(isCurrentlyFullscreen);
   };
+  const onVolumeChange = (value: number) => {
+    setVolume(+value);
+    if (videoRef?.current) {
+      videoRef.current.muted = value === 0;
+      videoRef.current.volume = +value * 0.01;
+    }
+  };
+  const toggleMute = () => {
+    const isMuted = volume === 0;
+
+    setVolume(isMuted ? 50 : 0);
+
+    if (videoRef?.current) {
+      videoRef.current.muted = !isMuted;
+      videoRef.current.volume = isMuted ? 0.5 : 0;
+    }
+  };
+
+  useEffect(() => {
+    onVolumeChange(0);
+  }, []);
 
   useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
   useTracks([Track.Source.Camera, Track.Source.Microphone])
@@ -40,6 +64,11 @@ export const LiveVideo = ({participant}: LiveVideoProps) => {
       <video ref={videoRef} width="100%" />
       <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
         <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <VolumeControl
+            onChange={onVolumeChange}
+            value={volume}
+            onToggle={toggleMute}
+          />
           <FullscreenControl
             isFullscreen={isFullscreen}
             onToggle={toggleFullscreen}
